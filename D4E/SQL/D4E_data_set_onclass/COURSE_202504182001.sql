af8bd83c197075e5d494268467a022c85f0dbed3
DK-Hieu <96808135+DK-Hieu@users.noreply.github.com>
2025-04-18 20:04:50 +0700

    update

---
+++ D4E/SQL/D4E_data_set_onclass/COURSE_202504182001.sql
@@ -0,0 +1,6 @@
+INSERT INTO COURSE (cID,cMajor,cName) VALUES
+	 (N'MC001',N'Data',N'D4E'),
+	 (N'MC002',N'Data',N'BI'),
+	 (N'MC003',N'Web',N'BE Basic'),
+	 (N'MC004',N'Web',N'Web Basic'),
+	 (N'MC005',N'Web',N'FullStack');
---
+++ D4E/SQL/D4E_data_set_onclass/TEACHERS_202504182002.sql
@@ -0,0 +1,4 @@
+INSERT INTO MIndX_Lec_1.dbo.TEACHERS (tID,tFirstName,tLastName,tPhone,tType,tMajor) VALUES
+	 (N'T0003',N'Khoa',N'Dao',N'0113656789',0,N'Data'),
+	 (N'T0005',N'Nghia',N'Cao',N'0562456590',0,N'Data'),
+	 (N'T0006',N'Ha',N'San',N'0783456459',1,N'Data');
